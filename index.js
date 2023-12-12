077250c89158e8a1d25cce209a15e54cc79a62e0
ehmicky <ehmicky@users.noreply.github.com>
2023-12-12 22:16:07 +0000

    Require Node.js 18 (#7)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 export default function stripFinalNewline(input) {
-	const LF = typeof input === 'string' ? '\n' : '\n'.charCodeAt();
-	const CR = typeof input === 'string' ? '\r' : '\r'.charCodeAt();
+	const LF = typeof input === 'string' ? '\n' : '\n'.codePointAt();
+	const CR = typeof input === 'string' ? '\r' : '\r'.codePointAt();
 
-	if (input[input.length - 1] === LF) {
+	if (input.at(-1) === LF) {
 		input = input.slice(0, -1);
 	}
 
-	if (input[input.length - 1] === CR) {
+	if (input.at(-1) === CR) {
 		input = input.slice(0, -1);
 	}
 
